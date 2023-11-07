7d351a6515a404fa9ada1aaecb8d31077f67004f
Darien Romero <darien@evolbit.net>
2023-11-07 09:39:40 -0500

    create user advance

--- src/app/user/components/create-user/create-user.component.ts
+++ src/app/user/components/create-user/create-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { User } from '../../interfaces/user.interface';
+import { AppService } from 'src/app/app/services/app.service';
+import { UserService } from '../../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'create-user',
@@ -6,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent {
+  public userToCreate: User = {
+    email:        "",
+    enabled:      true,
+    id:           "",
+    username:     "",
+    apps_enabled: []
+  };
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ){
 
+  }
+
+  get apps(){
+    return this.appService.apps
+  }
+
+  onChangeSelectedApp(appId: string){
+    const included = this.userToCreate.apps_enabled.includes(appId);
+    if(included){
+      this.userToCreate.apps_enabled = this.userToCreate.apps_enabled.filter(e => e != appId);
+    }else{
+      this.userToCreate.apps_enabled.push(appId);
+    }
+    console.log("new user", this.userToCreate);
+  }
+
+  createUser(){
+    this.userService.updateUser(this.userToCreate).then((result) => {
+      this.router.navigateByUrl("/users")
+    }).catch((err) => {
+      
+    });;
+  }
 }
